200a1d0a56141812e60da35406db4b6f80974b7f
Pavlo-V-M <114091610+Pavlo-V-M@users.noreply.github.com>
2023-03-20 20:00:59 +0000

    Deploying to gh-pages from @ Pavlo-V-M/goit-js-hw-09@b4b1036d587ffccd4b6b3c01d6bbc9eaaf09d97b 🚀

---
+++ 01-color-switcher.1b229246.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");function o(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}e.addEventListener("click",(function(){t||(t=setInterval(o,1e3))})),n.addEventListener("click",(function(){t=clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.1b229246.js.map
